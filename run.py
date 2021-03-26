602565cc6516f4545393d3cc850e3a36604403a5
zlataovce <34477304+zlataovce@users.noreply.github.com>
2021-03-26 20:21:20 +0100

    added direct text uploading

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, abort, Response, render_template
 from libs.parselib import Parse
 from libs.pastes import Paste
-from json import dumps
+from json import dumps, loads
 from os import remove
 import requests
 
@@ -19,7 +19,11 @@ def parse():
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
+
+@app.route("/text")
+def index2():
+    return render_template("textupload.html")
 
 
 @app.route("/show", methods=["GET","POST"])
@@ -27,12 +31,24 @@ def show():
     if request.method == "GET":
         index()
     if request.method == "POST":
-        r = requests.get("https://plshelp.mkdev.ml/api/v1?url={0}".format(request.form.get("url")))
         if r.status_code != 400:
             re = r.json()
             return render_template("show.html", plugins=re["plugins"], errors=re["errors"], minecraft_version=re["minecraft_version"], server_software=re["server_software"], reload=re["reload"], needs_newer_java=re["needs_newer_java"])
         else:
             return "The paste URL was wrong!", 400
+
+
+@app.route("/showv2", methods=["GET","POST"])
+def showv2():
+    if request.method == "GET":
+        index2()
+    if request.method == "POST":
+        with open("latest.log", "w") as f:
+            f.write(request.form.get("logfile"))
+        parser = Parse("latest.log")
+        re = parser.analysis()
+        remove('latest.log')
+        return render_template("show.html", plugins=re["plugins"], errors=re["errors"], minecraft_version=re["minecraft_version"], server_software=re["server_software"], reload=re["reload"], needs_newer_java=re["needs_newer_java"])
 
 
 app.run(host="0.0.0.0", port=8080)
