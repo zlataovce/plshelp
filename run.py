0d6febb0bf68f8f1f33279fa8b8f2714a88beada
JustGreg <inbox@grzegorzkibitz.com>
2021-03-30 09:58:38 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, abort, Response, render_template
+  
+from flask import Flask, request, Response, render_template
 from libs.parselib import Parse
 from libs.pastes import Paste
 from json import dumps
@@ -8,32 +9,45 @@ from pbwrap import Pastebin
 from dotenv import load_dotenv
 from libs.utils import check_filename
 from configparser import ConfigParser
+import concurrent
 
 load_dotenv()
 
 app = Flask('')
+
+
+def upload_paste_thread(text):
+    config = ConfigParser()
+    config.read('config.ini')
+    pb = Pastebin(api_dev_key=environ.get('PASTEBIN_API_KEY'))
+    pasteurl = pb.create_paste(text, api_paste_expire_date=config['SHARE']['PasteExpire'],
+                               api_paste_name=config['SHARE']['PasteTitle'])
+    return pasteurl
+
 
 @app.route('/api/v1')
 def parse():
     url = request.args.get("url")
     paster = Paste(url)
     if paster.identify() is False:
-        return abort(400)
+        return "Needs URL parameter.", 400
     parser = Parse(paster.filename)
     data = dumps(parser.analysis(), indent=2)
     remove(paster.filename)
     return Response(data, mimetype='application/json')
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/text")
 def index2():
     return render_template("textupload.html")
 
 
-@app.route("/show", methods=["GET","POST"])
+@app.route("/show", methods=["GET", "POST"])
 def show():
     config = ConfigParser()
     config.read('config.ini')
@@ -43,7 +57,10 @@ def show():
             shareurl = config['FLASK']['Domain'] + "/show?type=share&url=" + request.args.get("url")
             if r.status_code != 400:
                 re = r.json()
-                return render_template("show.html", plugins=re["plugins"], errors=re["errors"], minecraft_version=re["minecraft_version"], server_software=re["server_software"], reload=re["reload"], needs_newer_java=re["needs_newer_java"], share_url=shareurl, sbw_wrongshop=re["sbw_wrongshop"], paste_url=request.args.get("url", domain=config['FLASK']['Domain']))
+                return render_template("show.html", plugins=re["plugins"], errors=re["errors"],
+                                       minecraft_version=re["minecraft_version"], server_software=re["server_software"],
+                                       reload=re["reload"], needs_newer_java=re["needs_newer_java"], share_url=shareurl,
+                                       sbw_wrongshop=re["sbw_wrongshop"], paste_url=request.args.get("url"))
             else:
                 return "The paste URL was wrong!", 400
     if request.method == "POST":
@@ -51,16 +68,15 @@ def show():
         shareurl = config['FLASK']['Domain'] + "/show?type=share&url=" + request.form.get("url")
         if r.status_code != 400:
             re = r.json()
-            return render_template("show.html", plugins=re["plugins"], errors=re["errors"], minecraft_version=re["minecraft_version"], server_software=re["server_software"], reload=re["reload"], needs_newer_java=re["needs_newer_java"], share_url=shareurl, sbw_wrongshop=re["sbw_wrongshop"], paste_url=request.form.get("url"), domain=config['FLASK']['Domain'])
+            return render_template("show.html", plugins=re["plugins"], errors=re["errors"],
+                                   minecraft_version=re["minecraft_version"], server_software=re["server_software"],
+                                   reload=re["reload"], needs_newer_java=re["needs_newer_java"], share_url=shareurl,
+                                   sbw_wrongshop=re["sbw_wrongshop"], paste_url=request.form.get("url"))
         else:
             return "The paste URL was wrong!", 400
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-# note that we set the 404 status explicitly
-    return render_template('404.html'), 404
-@app.route("/showv2", methods=["GET","POST"])
+@app.route("/showv2", methods=["GET", "POST"])
 def showv2():
     config = ConfigParser()
     config.read('config.ini')
@@ -70,14 +86,18 @@ def showv2():
         filename = check_filename()
         with open(filename, "w") as f:
             f.write(request.form.get("logfile"))
-        pb = Pastebin(api_dev_key=environ.get('PASTEBIN_API_KEY'))
-        pasteurl = pb.create_paste(request.form.get("logfile"), api_paste_expire_date="2W", api_paste_name=config['SHARE']['PasteTitle'])
-        shareurl = config['FLASK']['Domain'] + "/show?type=share&url=" + pasteurl
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            thread = executor.submit(upload_paste_thread, request.form.get("logfile"))
         parser = Parse(filename)
         re = parser.analysis()
+        pasteurl = thread.result()
+        shareurl = config['FLASK']['Domain'] + "/show?type=share&url=" + pasteurl
         remove(filename)
         try:
-            return render_template("show.html", plugins=re["plugins"], errors=re["errors"], minecraft_version=re["minecraft_version"], server_software=re["server_software"], reload=re["reload"], needs_newer_java=re["needs_newer_java"], share_url=shareurl, sbw_wrongshop=re["sbw_wrongshop"], paste_url=pasteurl, domain=config['FLASK']['Domain'])
+            return render_template("show.html", plugins=re["plugins"], errors=re["errors"],
+                                   minecraft_version=re["minecraft_version"], server_software=re["server_software"],
+                                   reload=re["reload"], needs_newer_java=re["needs_newer_java"], share_url=shareurl,
+                                   sbw_wrongshop=re["sbw_wrongshop"], paste_url=pasteurl)
         except KeyError:
             return "Incomplete logs!", 400
 
