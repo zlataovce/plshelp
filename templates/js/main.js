25012da999243ccdde8c3d73715c4b6b3bce97bb
JustGreg <inbox@grzegorzkibitz.com>
2021-03-30 10:06:48 +0100

    Update main.js

--- templates/js/main.js
+++ templates/js/main.js
@@ -9,15 +9,15 @@
 };
 
 function toggleTheme() {
-    var footer = document.getElementById('footer')
+    var footer = document.getElementById("footer")
     footer.classList.toggle("footer-dark")
 
-    toggleClass(document.getElementById("navbar"), ['bg-dark', 'navbar-dark']);
+    toggleClass(document.getElementById("navbar"), ["bg-dark", "navbar-dark"]);
     
-    toggleClass(document.getElementById("body"), ['body-dark']);
-    toggleClass(document.getElementById('darkmode'), ['body-dark']);
-    toggleClass(document.getElementById('title'), ['title-dark']);
-    toggleClass(document.getElementById("parse"), ['btn-primary']);
-    toggleClass(document.getElementById('container'), ['container-dark']);
-    toggleClass(document.getElementById('subtitle'), ['subtitle-dark']);
+    toggleClass(document.getElementById("body"), ["body-dark"]);
+    toggleClass(document.getElementById("darkmode"), ["body-dark"]);
+    toggleClass(document.getElementById("title"), ["title-dark"]);
+    toggleClass(document.getElementById("parse"), ["btn-primary"]);
+    toggleClass(document.getElementById("container"), ["container-dark"]);
+    toggleClass(document.getElementById("subtitle"), ["subtitle-dark"]);
   }
