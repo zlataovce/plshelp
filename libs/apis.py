d9f7d88c0fd03559c9e63793e96f3271cc983381
zlataovce <34477304+zlataovce@users.noreply.github.com>
2021-04-10 17:38:37 +0200

    updated gravity and added regex

--- libs/apis.py
+++ libs/apis.py
@@ -1,14 +1,19 @@
 from requests import get
+import re
+
 
 def latest_commit_github(repo, branch):
 	url = "https://api.github.com/repos/" + repo + "/commits/" + branch
 	r = get(url).json()
 	return r["sha"][:7]
 
-def latest_build_jenkins(joburl):
+def latest_build_jenkins(joburl, regex=False):
 	url = joburl.rstrip("/") + "/lastSuccessfulBuild/api/json"
 	r = get(url).json()
-	return r["number"]
+	if regex is not False:
+		return re.search(regex, r["artifacts"][0]["displayPath"]).group(0)
+	else:
+		return r["number"]
 
 def latest_release_github(joburl):
 	url = "https://api.github.com/repos/" + joburl + "/releases/latest"
