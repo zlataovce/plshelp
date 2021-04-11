808c4505b78767eb39e820fc4a069a4d2be35672
zlataovce <34477304+zlataovce@users.noreply.github.com>
2021-04-11 19:12:51 +0200

    hotfixing connection errors

--- libs/apis.py
+++ libs/apis.py
@@ -1,15 +1,18 @@
-from requests import get
+import requests
 import re
 
 
 def latest_commit_github(repo, branch):
 	url = "https://api.github.com/repos/" + repo + "/commits/" + branch
-	r = get(url).json()
+	r = requests.get(url).json()
 	return r["sha"][:7]
 
 def latest_build_jenkins(joburl, regex=False):
 	url = joburl.rstrip("/") + "/lastSuccessfulBuild/api/json"
-	r = get(url).json()
+	try:
+		r = requests.get(url).json()
+	except requests.exceptions.ConnectionError:
+		return None
 	if regex is not False:
 		return re.search(regex, r["artifacts"][0]["displayPath"]).group(0)
 	else:
@@ -17,12 +20,12 @@ def latest_build_jenkins(joburl, regex=False):
 
 def latest_release_github(joburl):
 	url = "https://api.github.com/repos/" + joburl + "/releases/latest"
-	r = get(url).json()
+	r = requests.get(url).json()
 	return r["tag_name"]
 
 def latest_paper_build(minecraft_ver):
 	url = "https://papermc.io/api/v2/projects/paper/versions/" + minecraft_ver
-	r = get(url)
+	r = requests.get(url)
 	if r.status_code == 200:
 		return r.json()["builds"][-1]
 	else:
